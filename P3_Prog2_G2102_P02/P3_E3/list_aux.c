80883dbbb1c321a11184972c7ecba42f06e11326
sergamar <sergamar23@hotmail.es>
2017-04-01 19:22:59 +0200

    list_get missing

--- P3_Prog2_G2102_P02/P3_E3/list_aux.c
+++ P3_Prog2_G2102_P02/P3_E3/list_aux.c
@@ -23,4 +23,56 @@ ElementList* element_ini(){
     return ele;
 }
 
+/* Extrae del final de la lista realizando una copia del elemento almacenado en dicho nodo. */
+void * list_extractLast(List* list){
+    void * eleAux;
+    ElementList * eleFirst;
+    if (!list) {
+        fprintf(stderr, "list_extractLast: invalid arguments.\n");
+        return NULL;
+    }
+    eleFirst = list -> element; 
+    while(list->element->next->next){
+        list->element=list->element->next;
+    }
+    eleAux = list->copy_element_function(list->element->next->data);
+    if (!eleAux){
+        fprintf(stderr, "list_extractLast: error copying.\n");
+        return NULL;
+    }
+    list->element = eleFirst;
+    return eleAux;
+}
+/* Comprueba si una lista está vacía o no. */
+Bool list_isEmpty(const List* list){
+    if (!list || (list->element->data == NULL && list->element->next == NULL)){
+        return TRUE;
+    }
+    return FALSE;
+}
+/* Devuelve el elemento i-ésimo almacenado en la lista. En caso de error, devuelve NULL. */
+const void* list_get(const List* list, int i){
+    
+}
+/* Devuelve el tamaño de una lista. */
+int list_size(const List* list){
+    int aux = 0;
+    ElementList *eleAux;
+    if (!list){
+        fprintf(stderr, "list_size: invalid arguments.\n");
+        return -1;
+    }
+    if (list->element == NULL){
+        return 0;
+    }
+    eleAux = list->element;
+    while(list->element->next){
+        list->element=list->element->next;
+        aux++;
+    }
+    list->element = eleAux;
+    return aux;
+}
+/* Imprime una lista devolviendo el número de caracteres escritos. */
+int list_print(FILE *fd, const List* list);
 
